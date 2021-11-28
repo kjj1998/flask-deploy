97dc20247c93d2f84a82b852a1493d889ca7fa29
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-28 21:53:43 +0800

    Added code to retreieve config vars

--- student_list/__init__.py
+++ student_list/__init__.py
@@ -10,7 +10,9 @@ def create_app():
 	app = Flask(__name__)
 	app.config.from_mapping(
 			SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-			SQLALCHEMY_DATABASE_URI = "postgresql://postgres:151398@localhost/student" or \
+			SQLALCHEMY_DATABASE_URI = 
+				"postgres://juetylulrqtwpc:b2ba272e4ba7c331962556ece57a9ac3445e899122a5a684ceb3c5b2aed7eb7a@ec2-54-224-142-15.compute-1.amazonaws.com:5432/d5dabb50ftcdrh" or \
+				"postgresql://postgres:151398@localhost/student" or \
 				'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
 			SQLALCHEMY_TRACK_MODIFICATIONS = False,
 			UPLOAD_FOLDER = '.\\static\\uploads',
