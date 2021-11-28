21a0c7c1b5efcb49e7a13859186555c303ae738a
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-28 23:09:16 +0800

    Changed file paths to suit Linux

--- student_list/__init__.py
+++ student_list/__init__.py
@@ -13,16 +13,25 @@ def create_app():
 	if uri is not None:
 		if uri.startswith("postgres://"):
 			uri = uri.replace("postgres://", "postgresql://", 1)
+	else:
+		uri = "postgresql://postgres:151398@localhost/student"
+
 	app.config.from_mapping(
 			SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-			SQLALCHEMY_DATABASE_URI = 
-				uri or \
-				"postgresql://postgres:151398@localhost/student" or \
-				'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
+			SQLALCHEMY_DATABASE_URI = uri,
 			SQLALCHEMY_TRACK_MODIFICATIONS = False,
-			UPLOAD_FOLDER = '.\\static\\uploads',
-			DOWNLOAD_FOLDER = '.\\static\\downloads'
 	)
+
+	if uri == "postgresql://postgres:151398@localhost/student":
+		UPLOAD_FOLDER = '.\\static\\uploads',
+		DOWNLOAD_FOLDER = '.\\static\\downloads'
+	else:
+		UPLOAD_FOLDER = './static/uploads',
+		DOWNLOAD_FOLDER = './static/downloads'
+
+	app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
+	app.config['DOWNLOAD_FOLDER'] =  DOWNLOAD_FOLDER
+
 	db.init_app(app)
 	migrate.init_app(app, db)
 
